5bd8d2809f6478380dc0eb925186c5d2e7c0268a
tray <onesimus.githua@student.moringaschool.com>
2023-12-05 07:35:11 +0300

    created a stacks file

--- Assesments/Stacks.py
+++ Assesments/Stacks.py
@@ -1,29 +1,29 @@
-def is_balanced(expression):
-    l = []
-    condition = True
+# def is_balanced(expression):
+#     l = []
+#     condition = True
     
-    if len(expression) % 2 != 0:
-        return False
+#     if len(expression) % 2 != 0:
+#         return False
     
-    for c in expression:
-        if condition:
-            if c in '{[(':
-                l.append(c)
-            elif c in '}])' and len(l) > 0:
-                last = l.pop()
-                match c:
-                    case '}':
-                        condition = '{' == last
-                    case ']':
-                        condition = '[' == last
-                    case ')':
-                        condition = '(' == last
-            else:
-                condition = False
+#     for c in expression:
+#         if condition:
+#             if c in '{[(':
+#                 l.append(c)
+#             elif c in '}])' and len(l) > 0:
+#                 last = l.pop()
+#                 match c:
+#                     case '}':
+#                         condition = '{' == last
+#                     case ']':
+#                         condition = '[' == last
+#                     case ')':
+#                         condition = '(' == last
+#             else:
+#                 condition = False
     
-    return condition
+#     return condition
 
-expression1 = "([]{})"
-expression2 = "([)]"
-print(is_balanced(expression1))  # Output: True
-print(is_balanced(expression2))  # Output: False 
+# expression1 = "([]{})"
+# expression2 = "([)]"
+# print(is_balanced(expression1))  # Output: True
+# print(is_balanced(expression2))  # Output: False 
